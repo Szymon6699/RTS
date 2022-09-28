d1b3addf07c85c9cf97b769e23cd8d12bdd64530
szymon6699 <szymnow2112@gmail.com>
2022-09-28 15:50:10 +0200

    eksportBZ

--- RTS.sql
+++ RTS.sql
@@ -1,15 +1,17 @@
-INSERT INTO players (name,email,password) VALUES ("kacper","kacper1@gmail.com","haslo123")
-INSERT INTO players (name,email,password) VALUES ("Jan","jan123@gmail.com","jan123")
-INSERT INTO players (name,email,password) VALUES ("adam","adam1223@gmail.com","adam1223")
-INSERT INTO resources (name,description) VALUES ("drewno","surowiec")
-INSERT INTO resources (name,description) VALUES ("glina","surowiec")
-INSERT INTO resources (name,description) VALUES ("żelazo","surowiec")
-INSERT INTO buildings (name) VALUES ("dół z glinu")
-INSERT INTO buildings (name) VALUES ("chata drwala")
-INSERT INTO buildings (name) VALUES ("kowal")
-INSERT INTO troops (name,description,damage,dinstance_damage,defense) VALUES ("łucznik","STRZELA","20","200","15")
-INSERT INTO troops (name,description,damage,dinstance_damage,defense) VALUES ("miecznik","walczy wręcz","10","20","30")
-INSERT INTO troops (name,description,damage,dinstance_damage,defense) VALUES ("kusznik","STRZELA","40","250","15")
-INSERT INTO troops (name,description,damage,dinstance_damage,defense) VALUES ("rycerz","walczy wręcz","40","20","55")
-#SELECT * FROM players;
+INSERT INTO players (name,email,password) VALUES ("kacper","kacper1@gmail.com","haslo123");
+INSERT INTO players (name,email,password) VALUES ("Jan","jan123@gmail.com","jan123");
+INSERT INTO players (name,email,password) VALUES ("adam","adam1223@gmail.com","adam1223");
+INSERT INTO resources (name,description) VALUES ("drewno","surowiec");
+INSERT INTO resources (name,description) VALUES ("glina","surowiec");
+INSERT INTO resources (name,description) VALUES ("żelazo","surowiec");
+INSERT INTO buildings (name) VALUES ("dół z glinu");
+INSERT INTO buildings (name) VALUES ("chata drwala");
+INSERT INTO buildings (name) VALUES ("kowal");
+INSERT INTO troops (name,description,damage,dinstance_damage,defense) VALUES ("łucznik","STRZELA","20","200","15");
+INSERT INTO troops (name,description,damage,dinstance_damage,defense) VALUES ("miecznik","walczy wręcz","10","20","30");
+INSERT INTO troops (name,description,damage,dinstance_damage,defense) VALUES ("kusznik","STRZELA","40","250","15");
+INSERT INTO troops (name,description,damage,dinstance_damage,defense) VALUES ("rycerz","walczy wręcz","40","20","55");
+INSERT INTO Villages (name) VALUES ("wioska12");
+INSERT INTO Villages (name) VALUES ("wioska13");
+INSERT INTO Villages (name) VALUES ("wioska11");
 
